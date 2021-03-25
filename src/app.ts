0b2c855f84e09ebc3d84380f693113cc88e3b865
Keith Sharp <kms@passback.co.uk>
2021-03-25 11:19:49 +0000

    Added eslint support and fixed errors.

--- src/app.ts
+++ src/app.ts
@@ -4,31 +4,31 @@ class App {
     constructor() {
         const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
         
-        let engine = new Engine(canvas, true);
-        let scene = new Scene(engine);
+        const engine = new Engine(canvas, true);
+        const scene = new Scene(engine);
         scene.clearColor = new Color4(0, 0, 0, 1);
         
-        let camera = new FreeCamera("camera", new Vector3(0, 5, -10), scene);
+        const camera = new FreeCamera("camera", new Vector3(0, 5, -10), scene);
         camera.setTarget(Vector3.Zero());
         camera.attachControl(canvas, true);
         
-        let quadOrigin = new Vector3(-4, 0, 0);
-        let quadDestination = new Vector3(-2, 0, 0);
-        let quadControl = new Vector3(-3, 2, 0);
-        let quadPoints = 30;
+        const quadOrigin = new Vector3(-4, 0, 0);
+        const quadDestination = new Vector3(-2, 0, 0);
+        const quadControl = new Vector3(-3, 2, 0);
+        const quadPoints = 30;
 
-        let quadBezierVectors = Curve3.CreateQuadraticBezier(quadOrigin, quadControl, quadDestination, quadPoints);
-        let quadBezierCurve = Mesh.CreateLines("quadBezier", quadBezierVectors.getPoints(), scene);
+        const quadBezierVectors = Curve3.CreateQuadraticBezier(quadOrigin, quadControl, quadDestination, quadPoints);
+        const quadBezierCurve = Mesh.CreateLines("quadBezier", quadBezierVectors.getPoints(), scene);
         quadBezierCurve.color = new Color3(1, 0, 0);
 
-        let cubicOrigin = new Vector3(2, 0, 0);
-        let cubicDestination = new Vector3(4, 0, 0);
-        let cubicControlOne = new Vector3(2.5, 2, 0);
-        let cubicControlTwo = new Vector3(3.5, 2, 0);
-        let cubicPoints = 30;
+        const cubicOrigin = new Vector3(2, 0, 0);
+        const cubicDestination = new Vector3(4, 0, 0);
+        const cubicControlOne = new Vector3(2.5, 2, 0);
+        const cubicControlTwo = new Vector3(3.5, 2, 0);
+        const cubicPoints = 30;
 
-        let cubicBezierVectors = Curve3.CreateCubicBezier(cubicOrigin, cubicControlOne, cubicControlTwo, cubicDestination, cubicPoints);
-        let cubicBezierCurve = Mesh.CreateLines("cubicBezier", cubicBezierVectors.getPoints(), scene);
+        const cubicBezierVectors = Curve3.CreateCubicBezier(cubicOrigin, cubicControlOne, cubicControlTwo, cubicDestination, cubicPoints);
+        const cubicBezierCurve = Mesh.CreateLines("cubicBezier", cubicBezierVectors.getPoints(), scene);
         cubicBezierCurve.color = new Color3(0, 0, 1);
 
         engine.runRenderLoop(() => {
